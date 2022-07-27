ba0bf9bd37084f4c3bf1668cc1c353f3791e832e
XuJin <1395478584@qq.com>
2022-07-27 17:12:06 +0800

    2022年7月27日16:12:54

--- product/product-service/src/main/java/com/sa/ProductApplication.java
+++ product/product-service/src/main/java/com/sa/ProductApplication.java
@@ -1,9 +1,13 @@
 package com.sa;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.sa.mapper.mybaits")
@@ -12,4 +16,5 @@ public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class,args);
     }
+
 }
--- product/product-service/src/test/java/com/sa/test/ProductTest.java
+++ product/product-service/src/test/java/com/sa/test/ProductTest.java
@@ -3,6 +3,7 @@ package com.sa.test;
 import com.sa.ProductApplication;
 import com.sa.product.api.business.IProductService;
 import com.sa.product.dto.ProductDTO;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,13 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {ProductApplication.class})
 public class ProductTest {
 
     @Autowired
     private IProductService productService;
-
+    @Test
     public void testXing(){
         List<ProductDTO> all = productService.getAll();
         System.out.println(all.size());
